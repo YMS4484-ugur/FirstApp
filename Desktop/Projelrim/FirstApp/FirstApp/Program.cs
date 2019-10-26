32fa31a648f99b5fd249a437d949e0e6f3a5a1fe
uguresref <ugur.donmez93@outlook.com>
2019-10-26 12:22:53 +0300

    Initialize

---
+++ Desktop/Projelrim/FirstApp/FirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            // Console.ReadKey();
+            //Console.ReadLine();
+            Console.WriteLine("Lütfen Adınızı Giriniz");
+            Console.WriteLine("Giriş :" + Console.ReadLine());
+
+            Console.Read();
+        }
+    }
+}
