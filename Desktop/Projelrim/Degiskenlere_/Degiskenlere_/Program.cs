97c1313e7a5f0ee059a29c10c889f28bdf46130c
uguresref <ugur.donmez93@outlook.com>
2019-10-26 14:02:57 +0300

    Degiskenler_

--- Desktop/Projelrim/Degiskenlere_/Degiskenlere_/Program.cs
+++ Desktop/Projelrim/Degiskenlere_/Degiskenlere_/Program.cs
@@ -10,6 +10,32 @@ namespace Degiskenlere_
     {
         static void Main(string[] args)
         {
+            //tam sayı veri tipleri
+            // byte min-0 max-255
+            byte s1 = 255;  // unsigned varsa 0 dan başlar- değer olmaz
+            s1 = 12;
+            // sbyte
+            //min -128 max 128
+            sbyte s2 = 123;
+
+            //short
+            short b1 = 10;
+         //ushort
+         //int
+         //uint
+         //long
+         //ulong
+
+
+            //ondalıklı veri tipleri
+            //float
+            //double
+            //decimal
+            //metinsel veri tipleri
+            //char
+            //sttring
+            //mantıklsal veri tipi
+            //bool
         }
     }
 }
