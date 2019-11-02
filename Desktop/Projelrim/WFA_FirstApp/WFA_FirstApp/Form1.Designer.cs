95ab9f362273b3fbea774aa0a04e4b4f12f7449e
uguresref <ugur.donmez93@outlook.com>
2019-11-02 11:59:04 +0300

    Giris

---
+++ Desktop/Projelrim/WFA_FirstApp/WFA_FirstApp/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WFA_FirstApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbAvatarImage = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPasword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.chkRememberMe = new System.Windows.Forms.CheckBox();
+            this.lnkForgetPassword = new System.Windows.Forms.LinkLabel();
+            this.lnkNewAccount = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAvatarImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbAvatarImage
+            // 
+            this.pcbAvatarImage.Image = global::WFA_FirstApp.Properties.Resources.beautiful_neytiri_in_avatar_1280x1024;
+            this.pcbAvatarImage.Location = new System.Drawing.Point(157, 37);
+            this.pcbAvatarImage.Name = "pcbAvatarImage";
+            this.pcbAvatarImage.Size = new System.Drawing.Size(303, 229);
+            this.pcbAvatarImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbAvatarImage.TabIndex = 0;
+            this.pcbAvatarImage.TabStop = false;
+            this.pcbAvatarImage.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(256, 288);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(194, 22);
+            this.txtUserName.TabIndex = 3;
+            this.txtUserName.TextChanged += new System.EventHandler(this.TxtUserName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(173, 312);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre :";
+            // 
+            // txtPasword
+            // 
+            this.txtPasword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPasword.Location = new System.Drawing.Point(256, 316);
+            this.txtPasword.Name = "txtPasword";
+            this.txtPasword.Size = new System.Drawing.Size(194, 22);
+            this.txtPasword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(256, 344);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(195, 32);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "GİRİŞ";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // chkRememberMe
+            // 
+            this.chkRememberMe.AutoSize = true;
+            this.chkRememberMe.Location = new System.Drawing.Point(256, 383);
+            this.chkRememberMe.Name = "chkRememberMe";
+            this.chkRememberMe.Size = new System.Drawing.Size(92, 17);
+            this.chkRememberMe.TabIndex = 5;
+            this.chkRememberMe.Text = "Beni Hatırla";
+            this.chkRememberMe.UseVisualStyleBackColor = true;
+            // 
+            // lnkForgetPassword
+            // 
+            this.lnkForgetPassword.AutoSize = true;
+            this.lnkForgetPassword.Location = new System.Drawing.Point(256, 407);
+            this.lnkForgetPassword.Name = "lnkForgetPassword";
+            this.lnkForgetPassword.Size = new System.Drawing.Size(96, 13);
+            this.lnkForgetPassword.TabIndex = 6;
+            this.lnkForgetPassword.TabStop = true;
+            this.lnkForgetPassword.Text = "Şifremi Unuttum";
+            // 
+            // lnkNewAccount
+            // 
+            this.lnkNewAccount.AutoSize = true;
+            this.lnkNewAccount.Location = new System.Drawing.Point(256, 420);
+            this.lnkNewAccount.Name = "lnkNewAccount";
+            this.lnkNewAccount.Size = new System.Drawing.Size(116, 13);
+            this.lnkNewAccount.TabIndex = 7;
+            this.lnkNewAccount.TabStop = true;
+            this.lnkNewAccount.Text = "Yeni Hesap Oluştur";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 469);
+            this.Controls.Add(this.lnkNewAccount);
+            this.Controls.Add(this.lnkForgetPassword);
+            this.Controls.Add(this.chkRememberMe);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPasword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pcbAvatarImage);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAvatarImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbAvatarImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPasword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox chkRememberMe;
+        private System.Windows.Forms.LinkLabel lnkForgetPassword;
+        private System.Windows.Forms.LinkLabel lnkNewAccount;
+    }
+}
+
---
+++ Desktop/Projelrim/WFA_FirstApp/WFA_FirstApp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_FirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TxtUserName_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPasword.Text;
+
+
+
+
+
+
+            MessageBox.Show("kullanıcı adınız:" + username + "şifreniz:" + password);
+        }
+    }
+}
