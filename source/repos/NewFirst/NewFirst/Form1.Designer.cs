8ef40a6db5f4719d0d2e200f9be3390be1e1e968
uguresref <ugur.donmez93@outlook.com>
2019-11-03 13:51:41 +0300

    at yarşı

---
+++ source/repos/NewFirst/NewFirst/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace NewFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pc3 = new System.Windows.Forms.PictureBox();
+            this.pc2 = new System.Windows.Forms.PictureBox();
+            this.pc1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pc3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-3, 572);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(955, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-3, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(955, 16);
+            this.label2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(-3, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(955, 16);
+            this.label3.TabIndex = 0;
+            // 
+            // pc3
+            // 
+            this.pc3.Image = global::NewFirst.Properties.Resources.horse5;
+            this.pc3.Location = new System.Drawing.Point(0, 419);
+            this.pc3.Name = "pc3";
+            this.pc3.Size = new System.Drawing.Size(181, 150);
+            this.pc3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pc3.TabIndex = 1;
+            this.pc3.TabStop = false;
+            // 
+            // pc2
+            // 
+            this.pc2.Image = global::NewFirst.Properties.Resources.horse3;
+            this.pc2.Location = new System.Drawing.Point(0, 268);
+            this.pc2.Name = "pc2";
+            this.pc2.Size = new System.Drawing.Size(184, 129);
+            this.pc2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pc2.TabIndex = 1;
+            this.pc2.TabStop = false;
+            // 
+            // pc1
+            // 
+            this.pc1.Image = global::NewFirst.Properties.Resources.horse1;
+            this.pc1.Location = new System.Drawing.Point(0, 96);
+            this.pc1.Name = "pc1";
+            this.pc1.Size = new System.Drawing.Size(184, 150);
+            this.pc1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pc1.TabIndex = 1;
+            this.pc1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Yarışı Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(947, 634);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pc3);
+            this.Controls.Add(this.pc2);
+            this.Controls.Add(this.pc1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pc3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pc1;
+        private System.Windows.Forms.PictureBox pc2;
+        private System.Windows.Forms.PictureBox pc3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ source/repos/NewFirst/NewFirst/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Random rnd = new Random();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            pc1.Left += rnd.Next(20);
+            pc2.Left += rnd.Next(10);
+            pc3.Left += rnd.Next(15);
+        }
+    }
+}
